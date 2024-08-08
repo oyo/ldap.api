ce97ac428726ad56887f39ed8198e116a9c9efed
Martin Rohrmeier <martin.ra.rohrmeier@bmw.de>
2024-08-08 15:04:47 +0200

    feat: add browsing features for UI

--- src/LdapQuery.ts
+++ src/LdapQuery.ts
@@ -19,6 +19,25 @@ export const prepareParams = (server: LdapConfig, base?: string, scope?: Scope, 
     sizeLimit: 100,
 })
 
+export const browse = async (server: LdapConfig, base: string) => {
+    const client = new Client({ url: server.url })
+    try {
+        await client.bind(server.username, server.password)
+        let childNodes, data
+        {
+            const { searchEntries } = await client.search(base, { scope: 'one', filter: '(objectClass=*)', attributes: ['dn'], sizeLimit: 100 } )
+            childNodes = searchEntries
+        }
+        {
+            const { searchEntries } = await client.search(base, { scope: 'base' } )
+            data = searchEntries
+        }
+        return { childNodes, data }
+    } finally {
+        await client.unbind()
+    }
+}
+
 export const query = async (server: LdapConfig, params: LdapSearchParams) => {
     const client = new Client({ url: server.url })
     try {
@@ -29,5 +48,6 @@ export const query = async (server: LdapConfig, params: LdapSearchParams) => {
         await client.unbind()
     }
 }
+
 
 export default { query }
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,33 @@
 import express, { Request, Response } from 'express'
 import { readConfig } from './Config'
-import { prepareParams, query, Scope } from './LdapQuery'
+import { browse, prepareParams, query, Scope } from './LdapQuery'
 
 const config = readConfig()
 const app = express()
 const port = config.port ?? 8001
+
+const browseRequest = async (req: Request, res: Response) => {
+    const { name, base } = req.params
+    const server = config.servers[name]
+    if (!server) {
+        res.status(404).json({ error: { message: 'LDAP server not found' } })
+        return
+    }
+    const params = prepareParams(
+        server,
+        !base || base === '_' ? undefined : base,
+    )
+    try {
+        const { childNodes, data } = await browse(
+            server,
+            params.base
+        )
+        res.json({ params, childNodes, data })
+    } catch (error) {
+        console.log(error)
+        res.json({ params, error })
+    }
+}
 
 const queryRequest = async (req: Request, res: Response) => {
     const { name, scope, base, filter, atts } = req.params
@@ -39,12 +62,19 @@ app.get(`${config.root}/`, (req: Request, res: Response) =>
             .filter((r: string) => r !== '')
     )
 )
+app.get(`${config.root}/browse`, (req: Request, res: Response) =>
+    res.json(
+        Object.keys(config.servers).map(name => `${config.root}/browse/${name}`)
+    )
+)
 app.get(`${config.root}/search`, (req: Request, res: Response) =>
     res.json(
         Object.keys(config.servers).map(name => `${config.root}/search/${name}`)
     )
 )
+app.get(`${config.root}/browse/:name`, browseRequest)
 app.get(`${config.root}/search/:name`, queryRequest)
+app.get(`${config.root}/browse/:name/:base`, browseRequest)
 app.get(`${config.root}/search/:name/:base`, queryRequest)
 app.get(`${config.root}/search/:name/:base/:scope`, queryRequest)
 app.get(`${config.root}/search/:name/:base/:scope/:filter`, queryRequest)
